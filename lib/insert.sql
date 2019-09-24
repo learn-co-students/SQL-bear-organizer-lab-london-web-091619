8f5b7764e8ae6c18977c98869b0c36842acbcc8c
idmyn <hello@davidmyno.rs>
2019-09-24 15:46:54 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ('Mr. Chocolate', 1, 'M', 'red', 'angry', 1),
+       ('Rowdy', 2, 'F', 'blue', 'angry', 0),
+       ('Tabitha', 3, 'F', 'yellow', 'happy', 1),
+       ('Sargeant Brown', 5, 'F', 'orange', 'angry', 1),
+       ('Melissa', 9, 'F', 'brown', 'angry', 1),
+       ('Grinch', 92, 'M', 'green', 'mean', 1),
+       ('Wendy', 8, 'F', 'pink', 'angry', 0),
+       (NULL, 7, 'M', 'red', 'angry', 0);
